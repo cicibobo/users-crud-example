ed8d0bf28431cfad63d5f3e7bab8e2069d22ed4d
cicibobo <ruslan.alekseyev@gmail.com>
2015-10-18 13:33:25 +0500

    Лист и Удаление Готовы

--- js/main.js
+++ js/main.js
@@ -6,6 +6,158 @@ $(function() {
 
     var requests = new Requests();
 
-    requests.test();
+    //requests.ajax({
+    //    url:"users",
+    //    method:"get",
+    //    data:{id:1},
+    //    success:function(data){
+    //        console.log(data);
+    //    }
+    //});
+    //
+    //requests.ajax({
+    //    url:"users",
+    //    method:"post",
+    //    data:  {
+    //        name: "Sem",
+    //        phone: "+994772011334",
+    //        email: "sem.alekseyev",
+    //        address: "Zahid Qasimov street",
+    //        site: "siteurl.com"
+    //    },
+    //    success:function(data){
+    //        console.log(data);
+    //    }
+    //});
+    //
+    //requests.ajax({
+    //    url:"users",
+    //    method:"put",
+    //    data: {
+    //        id:2,
+    //        name: "Alexis",
+    //        phone: "+994772011334",
+    //        email: "sem.alekseyev",
+    //        address: "Zahid Qasimov street",
+    //        site: "site-url.com"
+    //    },
+    //    success:function(data){
+    //        console.log(data);
+    //    }
+    //});
+    //
+    //requests.ajax({
+    //    url:"users",
+    //    method:"delete",
+    //    data: {id:3},
+    //    success:function(data){
+    //        console.log(data);
+    //    }
+    //});
+    //
+    //requests.ajax({
+    //    url:"users",
+    //    method:"get",
+    //    success:function(data){
+    //        console.log(data);
+    //    }
+    //});
+
+    drawList();
+
+    $(document).on("click",".edit",function(){
+        console.log("Edit",$(this).data("user-id"));
+        $(this).data("user-id");
+
+        $('#modal-id').modal();
+
+        requests.ajax({
+            url:"users",
+            method:"get",
+            data:{id:$(this).data("user-id")},
+            success:function(data){
+                $.each(data,function(index,value){
+                    $('#user-form #'+index).val(value);
+                });
+            }
+        });
+    });
+
+
+    $(document).on('click','.save',function(){
+       var method,formData = [],serializedData;
+        serializedData =  $('form#user-form').serializeArray();
+
+        $.each(serializedData,function(index,value){
+            console.log(value);
+             formData[value.name] = value.value;
+        });
+
+
+        console.log(formData);
+
+
+        if(formData.hasOwnProperty('id') && formData.id ){
+            method = "put";
+        }else{
+            method = "post";
+        }
+
+        console.log(method);
+
+           requests.ajax({
+            url:"users",
+            method:method,
+            data:formData,
+            success:function(data){
+               console.log("Added");
+                drawList();
+            }
+        });
+    });
+
+
+
+    $(document).on("click",".delete",function(){
+        requests.ajax({
+            url:"users",
+            method:"delete",
+            data: {id:$(this).data("user-id")},
+            success:function(data){
+               drawList();
+            }
+        });
+    });
+
+
+
+
+    function drawList(){
+       var el = "#user-list";
+        requests.ajax({
+            url:"users",
+            method:"get",
+            success:function(data){
+                $(el+" > tbody").empty();
+
+                if(data.length == 0 ){ $(el).hide(); }else{ $(el).show(); }
+
+                data.forEach(function(value){
+                    $(el).append("<tr>" +
+                        "<td>"+value.name+"</td>" +
+                        "<td>"+value.phone+"</td>" +
+                        "<td>"+value.email+"</td>" +
+                        "<td>"+value.address+"</td>" +
+                        "<td>"+value.site+"</td>" +
+                        "<td><button class='edit center-block btn-warning' data-user-id='"+value.id+"' > <span class='glyphicon glyphicon-edit'  ></span></td>" +
+                        "<td><button class='delete center-block btn-danger' data-user-id='"+value.id+"' ><span class='glyphicon glyphicon-remove' ></span></td>" +
+                        "</tr>");
+                });
+            }
+        });
+
+    }
+
+
 
 });
--- js/requests.js
+++ js/requests.js
@@ -3,35 +3,127 @@
  */
 
 
-function Requests (){
+function Requests() {
 
 
-    this.users = [
+    this.usersList = Array(
+        {
+            id: 1,
+            name: "Ruslan",
+            phone: "+994772011334",
+            email: "ruslan.alekseyev",
+            address: "Zahid Qasimov street",
+            site: "siteurl.com"
+        },
+        {
+            id: 2,
+            name: "Aleks",
+            phone: "+994772011334",
+            email: "ruslan.alekseyev",
+            address: "Zahid Qasimov street",
+            site: "siteurl.com"
+        },
+        {
+            id: 3,
+            name: "James",
+            phone: "+994772011334",
+            email: "ruslan.alekseyev",
+            address: "Zahid Qasimov street",
+            site: "siteurl.com"
+        },
+        {
+            id: 4,
+            name: "Jesica",
+            phone: "+994772011334",
+            email: "ruslan.alekseyev",
+            address: "Zahid Qasimov street",
+            site: "siteurl.com"
+        },
+        {
+            id: 5,
+            name: "Michel",
+            phone: "+994772011334",
+            email: "ruslan.alekseyev",
+            address: "Zahid Qasimov street",
+            site: "siteurl.com"
+        }
+    );
 
-        {id:1, name:"Ruslan", phone:"+994772011334", email:"ruslan.alekseyev", address:"Zahid Qasimov street", site:"siteurl.com" },
-        {id:1, name:"Aleks", phone:"+994772011334", email:"ruslan.alekseyev", address:"Zahid Qasimov street", site:"siteurl.com" },
-        {id:1, name:"James", phone:"+994772011334", email:"ruslan.alekseyev", address:"Zahid Qasimov street", site:"siteurl.com" },
-        {id:1, name:"Jesica", phone:"+994772011334", email:"ruslan.alekseyev", address:"Zahid Qasimov street", site:"siteurl.com" },
-        {id:1, name:"Michel", phone:"+994772011334", email:"ruslan.alekseyev", address:"Zahid Qasimov street", site:"siteurl.com" }
-    ];
 
+    this.ajax = function (obj) {
+        var response;
 
-
-    this.ajax(obj){
-
-
-
-        switch(obj.url){
-
-            case  "users" : this.users; break;
-            case  "user" : this.users[obj.data.id]; break;
-
-
+        switch (obj.url) {
+            case  "users" :
+                response = this.users(obj);
+                break;
         }
 
-
+        return obj.success(response);
     }
 
 
+    this.users = function (obj) {
+
+// GET single user Request
+        if (obj.method == 'get' && obj.hasOwnProperty("data") && obj.data.hasOwnProperty("id")) {
+            //console.log("GET single user Request");
+
+            return $.extend({}, this.getUserById(obj.data.id) );
+        }
+// GET all users Request
+        else if (obj.method == 'get') {
+            //console.log("GET all users Request");
+
+            return $.extend([], this.usersList);
+        }
+// POST Request
+        else if (obj.method == 'post') {
+            //console.log("POST Request");
+            var newId = this.usersList[this.usersList.length-1].id+1;
+            var newData = obj.data;
+            newData.id = newId;
+            this.usersList.push(newData);
+
+            return $.extend({}, newData) ;
+
+        }
+// PUT Request
+        else if (obj.method == 'put') {
+            //console.log("PUT Request");
+            var user = this.getUserById(parseInt(obj.data.id));
+            user = obj.data;
+
+            return $.extend({}, user);
+        }
+// DELETE Request
+        else if (obj.method == 'delete') {
+            //console.log("DELETE Request");
+
+            user = this.getUserById(parseInt(obj.data.id));
+            var index = this.usersList.indexOf(user);
+            var deletedObj = $.extend({}, user);
+            this.usersList.splice(index, 1);
+
+            return deletedObj;
+        }
+    }
+
+// Get User by Id
+    this.getUserById = function (id) {
+        for (var i = 0; i < this.usersList.length; i++) {
+            if (this.usersList[i].id == id) {
+                return this.usersList[i];
+            }
+        }
+        return false;
+    };
+
+// Map User By Id
+    this.mapUserList = function (func) {
+        for (var i = 0; i < this.usersList.length; i++) {
+            func(this.usersList[i]);
+        }
+    }
 
 }
